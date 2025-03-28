9e3318e74aaeb255bf826e2e1569e1e2e37a8de2
shaunvalentine <valensh1@yahoo.com>
2025-03-28 00:49:44 -0700

    Added box shadow to modal message upon successful creation of user account

--- frontend/components/Modal.tsx
+++ frontend/components/Modal.tsx
@@ -19,8 +19,6 @@ const styles = StyleSheet.create({
     height: '20%',
     padding: '1%',
     borderWidth: 2,
-    borderRadius: 15,
-    borderColor: colors.globalAlternateColor,
     justifyContent: 'center',
     position: 'absolute',
     top: '50%', // Moves the modal container halfway down the screen
@@ -28,6 +26,13 @@ const styles = StyleSheet.create({
     transform: [{ translateX: '-50%' }, { translateY: '-50%' }], // Shifts the modal left by half of its width to truly center it
     backgroundColor: colors.globalSecondaryColor,
     zIndex: 100,
+    boxShadow: '20',
+    borderRadius: 3,
+    borderColor: colors.globalAlternateColor,
+    shadowColor: 'black', // shadow color (black in this case)
+    shadowOffset: { width: 5, height: 5 }, // offset of the shadow (vertical shadow here)
+    shadowOpacity: 0.8, // opacity of the shadow
+    shadowRadius: 10, // blur radius of the shadow
   },
   message: {
     textAlign: 'center',
